9903ec28a9f94d8bf3c1aa15e8401d93bb193c4c
ren.meta@gmail.com <ren.meta@gmail.com>
2020-02-05 16:25:44 -0500

    Movement funcrion uses flying to traverse graph quickly

--- api.py
+++ api.py
@@ -2,12 +2,18 @@ import json
 import requests
 import random
 import time
+from graph import graph as graph
 # from decouple import config
 
 # SECRET_KEY = config('SECRET_KEY')
 token = "8f8f7f8962c63020496c4d1833059b8555b20305"
-graph = {}
+# graph = 
 
+# 59,60
+# n + 0 1
+# s - 0,1
+# w - 1,0
+# e + 1,0
 class Queue:
     def __init__(self):
         self.queue = []
@@ -28,8 +34,7 @@ if current['room_id'] not in graph:
   for direction in current['exits']:
       new_room[direction] = "?"
   graph[current['room_id']] = [new_room, current]
-# print("current",graph)
-current
+# current
 
 def bfs(starting_room, destination):
   queue = Queue()
@@ -86,34 +91,33 @@ def status_check():
 # status_check()
 
 def movement(direction, next_room_id=None):
-  url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/'
-  if next_room_id is not None:
-    time.sleep(current['cooldown'])
-    data = f'{{"direction":"{direction}","next_room_id": "{next_room_id}"}}'
+  if graph[current['room_id']][1]['terrain'] == 'CAVE':
+    url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/'
   else:
-    time.sleep(current['cooldown'])
+    url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/fly/'
+  if next_room_id is not None:
+    data = f'{{"direction":"{direction}","next_room_id": "{next_room_id}"}}'
+    if graph[next_room_id][1]['terrain'] == 'CAVE':
+      url = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/'
+  else:
     data = f'{{"direction":"{direction}"}}'
+  
   result = requests.post(url, data=data,
                          headers={'Content-Type':'application/json',
                                   'Authorization': f'Token {token}'}).json()
-      # Update room_id in graph
+  # Update room_id in graph
   inverse_directions = {"n": "s", "s": "n", "e": "w", "w": "e"}
-#   print("graph",graph)
-#   time.sleep(25)
-# time.sleep(15)
   if result['room_id'] not in graph:
-    graph[current['room_id']][0][next_move] = result['room_id']
     graph[result['room_id']] = {}
-    for direction in result['exits']:
-      graph[result['room_id']][direction] = "?"
+    for road in result['exits']:
+      graph[result['room_id']][road] = "?"
     new_room = {}
-    for direction in result['exits']:
-      new_room[direction] = "?"
-    new_room[inverse_directions[next_move]] = current['room_id']
+    for road in result['exits']:
+      new_room[road] = "?"
     graph[result['room_id']] = [new_room, result]
+  graph[current['room_id']][0][direction] = result['room_id']
+  graph[result['room_id']][0][inverse_directions[direction]] = current['room_id']
   print(result)
-  with open("data.json", "w") as write_file:
-    json.dump(graph, write_file)
   return result
 
 # bfs(current['room_id'], "?")
@@ -124,44 +128,44 @@ while True:
     current_exits = graph[current['room_id']][0]
     # if status_check()['encumbrance'] > 0:
       # If inventory getting too full, go sell at shop
-    print(bfs(current['room_id'], 1))
-    target = bfs(current['room_id'], 1)
-    print('Heading to shop..')
+    # print(bfs(current['room_id'], 1))
+    # target = bfs(current['room_id'], 1)
+    # print('Heading to shop..')
+    # for direction, room_id in current_exits.items():
+    #   if room_id == target:
+    #     next_move = direction
+    #     print("You are at the shop")
+    #     break
+    # if status_check()['gold'] >= 1000:
+    #   # Change name to get clue for Lambda coin
+    target = bfs(current['room_id'], 55)[1]
+    print('Heading to the Shrine..')
     for direction, room_id in current_exits.items():
       if room_id == target:
         next_move = direction
-        print("You are at the shop")
         break
-    # if status_check()['gold'] >= 1000:
-    #   # Change name to get clue for Lambda coin
-    #   target = bfs(current['room_id'], 55)[1]
-    #   print('Heading to the Wishing well..')
-    #   for direction, room_id in current_exits.items():
-    #     if room_id == target:
-    #       next_move = direction
-    #       break
     # else:
-    directions = []
-    for direction, room_id in current_exits.items():
-        # If adjacent room_id not visited yet, add that direction
-        if room_id == "?":
-            directions.append(direction)
-    if len(directions) == 0:
-        print('All adjacent rooms visited..')
-        target = bfs(current['room_id'], "?")[1]
-        for direction, room_id in current_exits.items():
-            if room_id == target:
-                next_move = direction
-                break
-        directions = [direction for direction in current_exits.keys()]
-        if len(directions) == 1:
-            next_move = directions[0]
-        else:
-            if prev_move is not None:
-                directions.remove(inverse_directions[prev_move])
-            next_move = random.choice(directions)
-    else:
-        next_move = random.choice(directions)
+    # directions = []
+    # for direction, room_id in current_exits.items():
+    #     # If adjacent room_id not visited yet, add that direction
+    #     if room_id == "?":
+    #         directions.append(direction)
+    # if len(directions) == 0:
+    #     print('All adjacent rooms visited..')
+    #     target = bfs(current['room_id'], "?")[1]
+    #     for direction, room_id in current_exits.items():
+    #         if room_id == target:
+    #             next_move = direction
+    #             break
+    #     directions = [direction for direction in current_exits.keys()]
+    #     if len(directions) == 1:
+    #         next_move = directions[0]
+    #     else:
+    #         if prev_move is not None:
+    #             directions.remove(inverse_directions[prev_move])
+    #         next_move = random.choice(directions)
+    # else:
+    #     next_move = random.choice(directions)
 
     # print(f'Next room: {current_exits[next_move]}')
     print(f'Room[{current["room_id"]}] to Room[{current_exits[next_move]}]')
@@ -170,6 +174,8 @@ while True:
       end_room = movement(next_move)
     else:
       # Use "Wise Explorer" buff
+      time.sleep(current['cooldown'])
+
       end_room = movement(next_move, current_exits[next_move])
     print(f"CD: {end_room['cooldown']}, {end_room['messages']}")
     # if len(end_room['items']) > 0:
